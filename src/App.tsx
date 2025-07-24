2b7adabfb7d7fde9a30acf8d5d3d80c20f24fdc9
jmoreno-m <jmorenom@tec.com>
2025-07-24 12:07:49 -0600

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ const client = generateClient<Schema>();
 
 function App() {
 
-  const { signOut } = useAuthenticator();
+  //const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -25,10 +26,11 @@ function App() {
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
   }
-
+  //<h1>My todos</h1>
   return (
     <main>
-      <h1>My todos</h1>
+       
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
--- src/main.tsx
+++ src/main.tsx
@@ -11,10 +11,34 @@ import '@aws-amplify/ui-react/styles.css';
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
+  /*
   <React.StrictMode>
     <Authenticator>
       <App />
     </Authenticator>
   </React.StrictMode>
+*/
+
+  <React.StrictMode>
+    <Authenticator
+      components={{
+        SignIn: {
+          Header() {
+            return (
+              <div style={{ textAlign: 'center', marginBottom: 16 }}>
+                <img src="/logo.png" alt="Logo"   style={{ width: 250, display: 'block', margin: '0 auto' }} />
+                <h2>Welcome! Please sign in.</h2>
+              </div>
+            );
+          }
+        }
+      }}
+    >
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+
+
+
+
 );
