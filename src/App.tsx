12a9c4113d3d7fecd35049704c2a4eaf52aede43
jmoreno-m <jmorenom@tec.com>
2025-07-25 11:40:02 -0600

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,34 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-import { useAuthenticator,
-  Flex,
-  Text,
-  Divider,
-  Menu,
-  MenuItem
- } from '@aws-amplify/ui-react';
-
+import { Text, Flex, Divider, Menu, MenuItem } from '@aws-amplify/ui-react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import { useUserAttributes } from './useUserAttributes';
 
-const client = generateClient<Schema>();
-
- 
-
-
-
 function App() {
-
-  //const { signOut } = useAuthenticator();
   const { signOut } = useAuthenticator();
   const { attributes } = useUserAttributes();
 
-  //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-
-
-
-  
-
-  //<h1>My todos</h1>
   return (
     <main>
-      <Menu
-        position="absolute"
-        top="1rem"
-        left="1rem"      
-      >
-        <MenuItem onClick={() => alert('Download')}>
-          Download
-        </MenuItem>
-        <MenuItem onClick={() => alert('Create a Copy')}>
-          Create a Copy
-        </MenuItem>
-        <MenuItem onClick={() => alert('Mark as Draft')}>
-          Mark as Draft
-        </MenuItem>
+      <Menu position="absolute" top="1rem" left="1rem">
+        <MenuItem onClick={() => alert('Download')}>Download</MenuItem>
+        <MenuItem onClick={() => alert('Create a Copy')}>Create a Copy</MenuItem>
+        <MenuItem onClick={() => alert('Mark as Draft')}>Mark as Draft</MenuItem>
         <Divider />
-        <MenuItem isDisabled onClick={() => alert('Delete')}>
-          Delete
-        </MenuItem>
-        <MenuItem onClick={() => alert('Sign out')}>
-          Sign out
-        </MenuItem>
+        <MenuItem isDisabled onClick={() => alert('Delete')}>Delete</MenuItem>
+        <MenuItem onClick={() => alert('Sign out')}>Sign out</MenuItem>
       </Menu>
-      <Flex 
+
+      <Flex
         position="absolute"
         top="1rem"
         right="1rem"
         direction="row"
         alignItems="center"
         gap="0.5rem"
-        >
+      >
         <Text>{attributes?.preferred_username}</Text>
-        <Divider 
-          orientation="vertical" />
+        <Divider orientation="vertical" />
         <button onClick={signOut}>Sign out</button>
-      </Flex>  
-
-      
+      </Flex>
     </main>
   );
 }
